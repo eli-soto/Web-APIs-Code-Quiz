09061df1dd30b01a4d1ba1cb08b8665f154edf19
eli-soto <142184979+eli-soto@users.noreply.github.com>
2023-10-04 21:46:26 -0400

    redo homework

--- workspace/myscripts.js
+++ workspace/myscripts.js
@@ -1,22 +1,80 @@
-function startHello() { 
-    setInterval(display0, 1000)
-
-    console.log("Hello, I am a function declaration.");
-  
-    console.log("-----------------------------------");
-} 
-
-// declareHello()
-
-var startBtnEl = document.getElementById("startBtn")
-console.log("startBtnEl",startBtnEl)
-
-
-startBtnEl.addEventListener("click", startHello);
-
+var startBtnEl = document.getElementById("startBtn"); // where startBtnEl
 var timerEl = document.getElementById("timer");
-    var count = 9
-    function display0() {
-      timerEl.innerHTML = count;
-      count = count -1
-    }
+var welcomeContainer = document.querySelector(".quiz-intro");
+var questionsContainer = document.querySelector(".questions-screen");
+var questionEl = document.querySelector(".question-element");
+var btnContainer = document.querySelector(".btn-container");
+var quizEndContainer = document.querySelector(".quiz-finished-screen");
+
+var count = 9;
+var currentIndex = 0;
+
+var questions = [
+  {
+    question: "What is json?",
+    options: ["A script", "javaScript Object Notation", "Object Programing Language", "None of the above"],
+    answer: "Object Programing Language"
+  },
+  {
+    question: "What is nodeJS?",
+    options: ["A script", "javaScript Object Notation", "javeScript Runtime Env", "None of the above"],
+    answer: "javeScript Runtime Env"
+  },
+  {
+    question: "What is HTML?",
+    options: ["Hypertech Multimedia Lang", "javaScript Object Notation", "Object Programing Language", "None of the above"],
+    answer: "Hypertech Multimedia Lang"
+  }
+];
+
+
+
+function displayTimer() {
+  var timer = setInterval(function() {
+    timerEl.textContent = "Timer: " + count; 
+    count--;
+    if(count <= 0) {
+      timerEl.textContent = "Timer: 00:00";
+    clearInterval(timer)
+    //endQuiz()
+  }
+}, 1000)
+}
+
+function startQuiz() {
+  welcomeContainer.style.display = "none";
+  questionsContainer.style.display = "block";
+  btnContainer.innerHTML = "";
+  questionEl.textContent = "";
+  questionEl.textContent = questions[currentIndex].question;
+  var choices = questions[currentIndex].options;
+  for (let i = 0; i < choices.length; i++) {
+    var btn = document.createElement("button");
+    btn.setAttribute("class", "btn")
+    btn.innerHTML = choices[i];
+    btn.onclick = optionsTarget;
+    btnContainer.appendChild(btn)
+  }
+}
+
+function optionsTarget(event) {
+  //alert(event.target.textContent)
+  currentIndex++;
+  if (currentIndex === questions.length ) {
+    endQuiz();
+  } else {
+    startQuiz();
+  }
+}
+
+function endQuiz() {
+    questionsContainer.style.display = "none";
+    quizEndContainer.style.display = "block";
+
+}
+
+startBtnEl.addEventListener("click", function() {
+  displayTimer();
+  startQuiz(); 
+});
+
