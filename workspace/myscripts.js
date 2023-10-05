ef30e6dfb8b291032372f882240c5e3924172bcc
eli-soto <142184979+eli-soto@users.noreply.github.com>
2023-10-04 23:06:13 -0400

    hw

--- workspace/myscripts.js
+++ workspace/myscripts.js
@@ -1,3 +1,4 @@
+// references to html elements 
 var startBtnEl = document.getElementById("startBtn"); // where startBtnEl
 var timerEl = document.getElementById("timer");
 var welcomeContainer = document.querySelector(".quiz-intro");
@@ -6,23 +7,24 @@ var questionEl = document.querySelector(".question-element");
 var btnContainer = document.querySelector(".btn-container");
 var quizEndContainer = document.querySelector(".quiz-finished-screen");
 
-var count = 9;
-var currentIndex = 0;
 
-var questions = [
+var count = 60; //why nine 
+var currentIndex = 0;  //makes sense starts count with, index of some array
+var timer 
+var questions = [            
   {
-    question: "What is json?",
-    options: ["A script", "javaScript Object Notation", "Object Programing Language", "None of the above"],
+    question: "What is json?",         //element //object  
+    options: ["A script", "javaScript Object Notation", "Object Programing Language", "None of the above"],   //array of objects
     answer: "Object Programing Language"
   },
   {
     question: "What is nodeJS?",
-    options: ["A script", "javaScript Object Notation", "javeScript Runtime Env", "None of the above"],
+    options: ["A script", "javaScript Object Notation", "javeScript Runtime Env", "None of the above"],        //element //object
     answer: "javeScript Runtime Env"
   },
   {
     question: "What is HTML?",
-    options: ["Hypertech Multimedia Lang", "javaScript Object Notation", "Object Programing Language", "None of the above"],
+    options: ["Hypertech Multimedia Lang", "javaScript Object Notation", "Object Programing Language", "None of the above"],  //element //object
     answer: "Hypertech Multimedia Lang"
   }
 ];
@@ -30,37 +32,37 @@ var questions = [
 
 
 function displayTimer() {
-  var timer = setInterval(function() {
-    timerEl.textContent = "Timer: " + count; 
-    count--;
-    if(count <= 0) {
+  timer = setInterval(function() {  //browswer api allows apps to communicate 
+    timerEl.textContent = "Timer: " + count; //
+    count--; //subtract 1 
+    if(count <= 0) {  
       timerEl.textContent = "Timer: 00:00";
     clearInterval(timer)
     //endQuiz()
   }
-}, 1000)
+}, 1000)  //function within set interval will be called every 1 secs
 }
 
 function startQuiz() {
   welcomeContainer.style.display = "none";
   questionsContainer.style.display = "block";
-  btnContainer.innerHTML = "";
-  questionEl.textContent = "";
-  questionEl.textContent = questions[currentIndex].question;
-  var choices = questions[currentIndex].options;
+  btnContainer.innerHTML = ""; //empty out
+  questionEl.textContent = ""; //empty out
+  questionEl.textContent = questions[currentIndex].question; //1st elements of the questions array with an object. and we are picking the value of the property(its akey) questions
+  var choices = questions[currentIndex].options; 
   for (let i = 0; i < choices.length; i++) {
-    var btn = document.createElement("button");
-    btn.setAttribute("class", "btn")
-    btn.innerHTML = choices[i];
-    btn.onclick = optionsTarget;
-    btnContainer.appendChild(btn)
+    var btn = document.createElement("button"); //create an element, button
+    btn.setAttribute("class", "btn") //creating an attribute class= btn
+    btn.innerHTML = choices[i];       //giving html content to our button. i=0 
+    btn.onclick = optionsTarget;       // ()=method no () is a method    //4 options
+    btnContainer.appendChild(btn)     //append each buttton add text content
   }
 }
 
 function optionsTarget(event) {
   //alert(event.target.textContent)
   currentIndex++;
-  if (currentIndex === questions.length ) {
+  if (currentIndex === questions.length ) {  //if condition is met 
     endQuiz();
   } else {
     startQuiz();
@@ -70,6 +72,7 @@ function optionsTarget(event) {
 function endQuiz() {
     questionsContainer.style.display = "none";
     quizEndContainer.style.display = "block";
+    clearInterval(timer)
 
 }
 
